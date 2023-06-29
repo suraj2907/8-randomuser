257fcb0335875505533ca12d04e238f3846f09b9
Suraj JAwrani <surajjawrani2022@gmail.com>
2023-06-30 00:18:49 +0530

    Extracted Data from API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState , useEffect} from "react";
+import { Container, Row , Col } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
+import MyCard from "./MyCard";
+
+import Axios from "axios";
+
 
 function App() {
+const [details , setDetails]= useState({});
+const fetchData = async () =>{
+   const response = await Axios.get("https://randomuser.me/api/");
+   const data = await response.data;
+   console.log(data);
+
+  //  const date = await response.data.dob.date ;
+  //  console.log(date);
+
+   const details = data.results[0];
+   setDetails(details);
+}
+
+useEffect(() => {
+fetchData();
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="p-4 App">
+      <Row >
+        <Col md={4} className="offset-md-4 mt-4">
+          <MyCard details = {details} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/MyCard.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import { FaPhone, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+
+const MyCard = ({ details }) => {
+  return (
+    <Card>
+      <CardBody className="text-center">
+        <img
+          height={150}
+          width={150}
+          className="rounded-circle border-danger img-thumbnail"
+          src={details.picture?.large}
+        />
+        <CardTitle className="text-primary">
+          <h1 className="pt-4">
+            <span> {details.name?.title} </span>
+            <span> {details.name?.first} </span>
+            <span> {details.name?.last} </span>
+          </h1>
+        </CardTitle>
+        <CardText>
+          <FaMapMarkedAlt />
+          <span style={{ paddingLeft: "10px" }}>
+            
+            {details.location?.street?.number}
+            <span> {details.location?.street?.name} </span>
+          </span>
+          <div>
+            <FaPhone />
+            <span style={{ paddingLeft: "10px" }}>{details.phone}</span>
+          </div>
+          <div>
+            <FaEnvelope />
+            <span style={{ fontSize: "25px", paddingLeft : "5px" }}> {details.email} </span>
+          </div>
+
+          <div>
+            <span style={{ color : "blue", fontWeight: "semi-bold"}}> Age: {details.dob?.age} </span>
+            <span style={{ color : "blue", fontWeight: "semi-bold"}}> DOB:
+              </span>
+          </div>
+        </CardText>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default MyCard;
